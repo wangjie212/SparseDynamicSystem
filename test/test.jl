a8f7adb41b3cbc013896b8f8c2e19b5cc6b4e437
wangjie212 <55985924+wangjie212@users.noreply.github.com>
2021-06-08 02:11:05 +0800

    Add files via upload

--- test/test.jl
+++ test/test.jl
@@ -1,27 +1,68 @@
-include("E:\\Programs\\SparseDynamicSystem\\SparseDynamicSystem\\src\\SparseDynamicSystem.jl")
-using .SparseDynamicSystem
+# include("E:\\Programs\\SparseDynamicSystem\\SparseDynamicSystem\\src\\SparseDynamicSystem.jl")
+using SparseDynamicSystem
 using DynamicPolynomials
+# using Plots
+using LinearAlgebra
+using LightGraphs
+using DelimitedFiles
 
 # The Lorentz system
-@polyvar x[1:3]
-f = [10*x[1]-12*x[2], -70/3*x[1]+x[2]+125/3*x[1]*x[3], 8/3*x[3]-15*x[1]*x[2]]
-g = [1-x[1]^2, 1-x[2]^2, 1-x[3]^2]
+@polyvar x[1:5]
+f = [10*x[1]-12*x[2], -70/3*x[1]+x[2]+125/3*x[1]*x[3], 8/3*x[3]-15*x[1]*x[2], 10*(x[4]-x[1]), x[1]*(28-x[3])-x[5]]
+g = [1-x[1]^2, 1-x[2]^2, 1-x[3]^2, 1-x[4]^2, 1-x[5]^2]
 @time begin
-opt,w = MPI(f, g, x, 3, -ones(3), ones(3), QUIET=true, TS=["block","block"], SO=[2;1], β=1)
+opt,w = MPI(f, g, x, 5, -ones(5), ones(5), QUIET=false, merge=true, md=3, TS=["block","MD"], SO=[1;1], β=1)
 end
+
+n = 6
+B = zeros(Float16, n, n)
+while eigmin(B) <= 0
+    G = erdos_renyi(n, n-4)
+    A = adjacency_matrix(G)
+    for j = 1:n, k = j:n
+        if j == k
+            B[j,k] = rand(1)[1]+1
+        elseif A[j,k] == 1
+            B[j,k] = rand(1)[1]-0.5
+            B[k,j] = B[j,k]
+        else
+            B[k,j],B[j,k] = 0,0
+        end
+    end
+end
+
+@polyvar x[1:n]
+f = -Diagonal(ones(n))*x + x'*B*x*x
+g = [1-x[i]^2 for i=1:n]
+@time begin
+opt,w = MPI(f, g, x, 5, -ones(n), ones(n), QUIET=false, merge=true, md=3, TS=["block","block"], SO=[2;1], β=1)
+end
+@time begin
+opt,w = MPI(f, g, x, 3, -ones(n), ones(n), QUIET=false, merge=true, md=3, TS=[false,false], SO=[1;1], β=1)
+end
+@time begin
+opt,w = MPI(f, g, x, 6, -ones(n), ones(n), QUIET=false, merge=true, md=2, TS=["block","block"], SO=[1;2], β=1)
+end
+
+io = open("E:\\Programs\\SparseDynamicSystem\\data\\random_10.txt", "w")
+writedlm(io, B)
+close(io)
+io = open("E:\\Programs\\SparseDynamicSystem\\data\\random_6.txt", "r")
+B = readdlm(io, Float16)
+close(io)
 
 # the Van-der-Pol oscillator
 @polyvar x[1:2]
 f = [2*x[2], -0.8*x[1] - 10*(x[1]^2-0.21)*x[2]]
 g = [1.1^2-x[1]^2, 1.1^2-x[2]^2]
 @time begin
-opt,w = MPI(f, g, x, 10, -1.1*ones(2), 1.1*ones(2), β=1)
+opt,w = MPI(f, g, x, 10, -1.1*ones(2), 1.1*ones(2), TS=["block","block"], SO=[3;1], β=1)
 end
 
 @polyvar x[1:6]
 f = [2*x[2], -0.8*x[1] - 10*(x[1]^2-0.21)*x[2] + 0.1*x[5], 2*x[4], -0.8*x[3] - 10*(x[3]^2-0.21)*x[4] + 0.1*x[1], 2*x[6], -0.8*x[5] - 10*(x[5]^2-0.21)*x[6] + 0.1*x[3]]
-g = [1.1^2-x[1]^2, 1.1^2-x[2]^2, 1.1^2-x[3]^2, 1.1^2-x[4]^2, 1.1^2-x[5]^2, 1.1^2-x[6]^2]
-opt,w = MPI(f, g, x, 4, -1.1*ones(6), 1.1*ones(6), TS=["block", "MD"])
+g = [1-x[1]^2, 1-x[2]^2, 1-x[3]^2, 1-x[4]^2, 1-x[5]^2, 1-x[6]^2]
+opt,w = MPI(f, g, x, 5, -ones(6), ones(6), TS=["block", "block"], SO=[2;1])
 
 # 9 mode fluid model
 α = 1/2
@@ -53,7 +94,7 @@ h = -(β^2*a[1]^2 + (4*β^2/3 + 1)*a[2]^2 + κ2^2*a[3]^2 + (3*α^2 + 4*β^2)/3*a
 # h = (1-a[1])^2 + sum(a[2:9].^2)
 
 @time begin
-opt = UPO(f, h, g, a, 3, TS="block")
+opt = UPO(f, h, g, a, 3, TS="block", SO=2)
 end
 
 # Powergrid 1
@@ -69,12 +110,23 @@ p = [1-x[1]^2, 1-x[2]^2, 1-x[3]^2, 1-x[4]^2, 1-x[5]^2, 1-x[6]^2]
 q = [0.01-sum(x.^2)]
 
 @time begin
-opt,w = MPI(f, p, x, 2, -ones(6), ones(6), β=1, TS=["block", "MD"])
+opt,w = MPI(f, p, x, 5, -ones(6), ones(6), β=1, SO=[1,1], merge=true, md=4, TS=["MD", "MD"])
 end
 
 @time begin
-opt,v0 = ROA(f, p, q, x, 8, 3, -ones(6), ones(6), TS=["MD","MD"])
+opt,v0 = ROA(f, p, q, x, 8, 3, -ones(6), ones(6), SO=[1,1], merge=true, md=4, TS=["block","block"])
 end
+
+@polyvar x[1:6]
+f = Vector{Polynomial}(undef, 6)
+f[1] = - x[3] - 0.5x[3]*x[6] + 0.5x[4]*x[5] - 0.4x[1]
+f[2] = - 0.5x[4] + 0.5x[3]*x[6] - 0.5x[4]*x[5] - 0.5x[2] + 0.05
+f[3] = x[1]*x[5]
+f[4] = x[2]*x[6]
+f[5] = - x[1]*x[3]
+f[6] = - x[2]*x[4]
+p = [1-x[1]^2, 1-x[2]^2, 1-x[3]^2, 1-x[4]^2, 1-x[5]^2, 1-x[6]^2]
+q = [0.01-sum(x.^2)]
 
 # 16 mode fluid model
 @polyvar a[1:16]
@@ -86,16 +138,28 @@ end
 f[16] = -(2*π*16)^2*a[16] - sqrt(2)*π*8*sum(a[j]*a[16-j] for j=1:15)
 h = 2*π^2*sum(i^2*a[i]^2 for i=1:16)
 
-t = zeros(9)
-val = zeros(9)
-Φ0 = 10^5
-for i = 1:9
-Φ = (i+1)*Φ0
-g = [h/Φ^(3/4) - Φ^(1/4)]
-o = [Φ^(1/4)/(2*π^2) - sum(a.^2)/Φ^(3/4)]
+t = zeros(10)
+val = zeros(10)
+Φ0 = 10^3
+for i = 1:10
+Φ = i*Φ0
+g = [h/Φ^(1/4) - Φ^(3/4)]
+o = [Φ^(3/4)/(2*π^2) - sum(a.^2)/Φ^(1/4)]
 
 t[i] = @elapsed begin
-opt = BEE(f, h, o, g, a, 2, TS=["block","block"], SO=[2,1], QUIET=false, merge=true, md=4)
+opt = BEE(f, h, o, g, a, 2, TS=["MD","MD"], SO=[1,1], merge=true, md=4, QUIET=false)
 end
 val[i] = opt/Φ^(3/2)
 end
+
+cp = subs(w, x[3:n] => zeros(n-2))
+a = Float16[]
+b = Float16[]
+for i=-1:0.002:1, j=-1:0.002:1
+    if  polynomial(cp)(x[1:2] => [i;j]) >= 1
+        push!(a, i)
+        push!(b, j)
+    end
+end
+plot(a, b, seriestype=:scatter, xlims=[-1;1], ylims=[-1;1], markersize=0.1, markercolor="blue", label=["TS, 2d=4"])
+savefig("E:\\Programs\\SparseDynamicSystem\\data\\random_12_2_TS.png")
